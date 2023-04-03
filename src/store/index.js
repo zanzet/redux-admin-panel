595e299e2e6e6bde8e3bd8eaf7118b43ad24ab69
Andrey <anddrejkaluzni@gmail.com>
2023-04-03 22:21:37 +0300

    enhenser

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,28 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import filters from '../reducers/filter';
 import heroes from '../reducers/heroes';
 
-const store = createStore( combineReducers({ heroes, filters }), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhenser = (createStore) => (...arg) =>{
+    const store = createStore(...arg);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) =>{
+        if (typeof action == 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store
+}
+
+const store = createStore(
+                combineReducers({ heroes, filters }),
+                compose(
+                    enhenser,
+                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                )
+ ) 
 
 export default store;
